4592939c26ca37f5b0184226e46d98bae2fd640a
jokingmoony <penubokim@gmail.com>
2022-08-08 23:12:03 +0900

    Add pngjs and test loading png files

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+import fs from 'fs';
+import { PNG } from 'pngjs/browser.js';
+
+function loadPNGImage(imagePath) {
+  return new Promise((res, _) => {
+    fs.createReadStream(imagePath)
+      .pipe(
+        new PNG({
+          filterType: 4,
+        })
+      ).on("parsed", function() {
+        res(this)
+      })
+  })
+}
+
+const image = await loadPNGImage('./sample/avocado.png')
+console.log('data', image.data)
+console.log(image.width, image.height)
