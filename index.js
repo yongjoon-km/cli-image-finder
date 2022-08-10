38741707b716bf4bef90405216d95e06cec93928
jokingmoony <penubokim@gmail.com>
2022-08-10 23:30:55 +0900

    Found png file iterating through directories

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { PNG } from 'pngjs/browser.js';
+import path from 'node:path';
 
 function loadPNGImage(imagePath) {
   return new Promise((res, _) => {
@@ -17,3 +18,21 @@ function loadPNGImage(imagePath) {
 const image = await loadPNGImage('./sample/avocado.png')
 console.log('data', image.data)
 console.log(image.width, image.height)
+
+// iterate files
+console.log("iterating files")
+console.log(process.env.PWD)
+
+function findPngFile(dir) {
+  const files = fs.readdirSync(dir)
+  for (const file of files) {
+    const filePath = `${dir}/${file}`
+    if (fs.statSync(filePath).isDirectory()) {
+      findPngFile(filePath)
+    } else if(filePath.endsWith('.png')) {
+      console.log('found', filePath)
+    }
+  }
+}
+
+findPngFile(process.env.PWD)
